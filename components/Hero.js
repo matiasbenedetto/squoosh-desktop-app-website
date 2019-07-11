a0309a62453208a96351c8ea6586694a8c9feed5
matias <matias.benedetto@gmail.com>
2019-07-11 20:16:14 -0300

    improving hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,36 +1,13 @@
 export default () => (
     <div className="hero">
-        <style jsx>{`
-        .hero{
-            background-image: url("static/img/stars2.jpg");
-            background-size: cover;
-            background-position: bottom;
-            height: 80vh;
-            display: flex;
-            justify-content:center;
-            overflow: hidden;
-        }
-        .container{
-            display: flex;
-            align-items: center;
-        }
-        h1{
-            color: white;
-        }
-        p{
-            color: white;
-        }
-        figure{
-            padding:0;
-        }
-      `}</style>
         <div className="container">
             <div className="text">
-                <h1 className="title is-1">Squoosh your images to get the best</h1>
-                <p className="subtitle is-5">Multiplatform Desktop app to compress, resize, and crop your images. <br/> It's completely free and for free!</p>
-                <button className="button is-primary is-large">Download</button>
+                <h1 className="title is-1 is-spaced">Squoosh your images to get the best</h1>
+                <p className="subtitle is-5">Multiplatform desktop app to optimize, resize, compress and convert your images.</p>
+                <p className="subtitle is-5">It's completely free and for free!</p>
+                <a href="#download" className="button is-link is-large">Download</a>
             </div>
-            <div className="">
+            <div>
                 <figure>
                     <img src="static/img/screenshot1.png" alt="Squoosh Desktop App screenshot" />
                 </figure>
