fe54515df041dd0aefe42bfab8785efb68165b84
matias <matias.benedetto@gmail.com>
2019-07-11 18:56:10 -0300

    adding footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,64 @@
 export default () => (
-    <footer className="has-background-light">
+    <footer className="">
+        <style jsx>{`
+            footer{
+                background-color: #333;
+                color: white;
+                padding: 60px 0;
+            }
+            h2{
+                color: white;
+                font-weight: normal;
+            }
+            h3{
+                color: white;
+            }
+            p{
+                margin-bottom: 1em;
+            }
+            .logo{
+                text-align: center;
+            }
+            .logo img{
+                max-width:200px;
+            }
+            a{
+                color: white;
+                text-decoration: underline;
+            }
+        `}</style>
+
         <div class="container">
             <div className="columns">
                 
                 <div className="column">
                     <div className="logo">
                         <img src="static/img/logo-1024x1024.png" />
-                        <b>Squoosh</b><br/>
-                        Desktop app
+                        <h2 className="title is-4"><b>Squoosh</b><br/>
+                        Desktop app</h2>
                     </div>
                 </div>
 
                 <div className="column">
-
+                    <h3 className="title is-4">Credits</h3>
+                    <p>Squoosh Desktop App is an port based on <a href="https://electronjs.org/" target="_blank">Electron</a> and working 100% offline of the online version of Squosh.</p>
+                    <p>Squoosh is and open source product made by the <a href="https://github.com/GoogleChromeLabs" target="_blank">Google Chrome Labs</a>.</p>
+                    <p>The desktop app is a project of <a href="http://mebendetto.com" target="_blank">Matias Benedetto</a></p>
                 </div>
 
                 <div className="column">
-
+                    <h3 className="title is-4">Contribute</h3>
+                    <p>Squoosh Desktop App is an open source project, you can contribute in <a href="https://github.com/matiasbenedetto/squoosh-desktop-app" target="_blank">GitHub</a>.</p>
                 </div>
 
                 <div className="column">
-
+                    <h3 className="title is-4">Links</h3>
+                    <p><a href="https://github.com/matiasbenedetto/squoosh-desktop-app" target="_blank">Project's repo</a></p>
+                    <p><a href="http://mebendetto.com" target="_blank">Author's blog</a></p>
+                    <p><a href="https://squoosh.app" target="_blank">Squoosh web app</a></p>
                 </div>
             </div>
         </div>
+
     </footer>
 )
