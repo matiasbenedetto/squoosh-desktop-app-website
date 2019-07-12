c4e86e1ab04505baf46d8b78afcccdd0ba1c167f
matias <matias.benedetto@gmail.com>
2019-07-12 20:38:08 -0300

    removing unnecesary markup

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,16 @@
 export default () => (
-    <div className="hero-wrapper">
-        <div className="hero">
-            <div className="container">
-                <div className="text">
-                    <h1 className="title is-1 is-spaced">Squoosh your images to get the best</h1>
-                    <p className="subtitle is-5">Multiplatform desktop app to optimize, resize, compress and convert your images.</p>
-                    <p className="subtitle is-5">It's completely free and for free!</p>
-                    <a href="#download" className="button is-link is-large">Download</a>
-                </div>
-                <div>
-                    <figure>
-                        <img src="static/img/screenshot1.png" alt="Squoosh Desktop App screenshot" />
-                    </figure>
-                </div>
+    <div className="hero">
+        <div className="container">
+            <div className="text">
+                <h1 className="title is-1 is-spaced">Squoosh your images to get the best</h1>
+                <p className="subtitle is-5">Multiplatform desktop app to optimize, resize, compress and convert your images.</p>
+                <p className="subtitle is-5">It's completely free and for free!</p>
+                <a href="#download" className="button is-link is-large">Download</a>
+            </div>
+            <div>
+                <figure>
+                    <img src="static/img/screenshot1.png" alt="Squoosh Desktop App screenshot" />
+                </figure>
             </div>
         </div>
     </div>
