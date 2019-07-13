3977f2a10b3478e2cc17a50926aa6d0e58d6f81d
matias <matias.benedetto@gmail.com>
2019-07-12 21:11:32 -0300

    correcting metatag

--- components/MetaTags.js
+++ components/MetaTags.js
@@ -18,7 +18,7 @@ export default () => (
 
         <meta name="og:title" content="Squoosh Desktop App"/>
         <meta name="og:description" content="Squoosh your images to get the best | Multiplatform desktop app to optimize, resize, compress and convert your images."/>
-        <meta name="og:image" content="https://squoosh-desktop.now.sh/static/img/logo-1024x1024.png"/>
+        <meta name="og:image" content="https://squoosh-desktop.now.sh/gistatic/img/screenshot1.png"/>
         <meta name="og:url" content="https://squoosh-desktop.now.sh/"/>
         <meta name="og:site_name" content="Squoosh Desktop App"/>
         <meta name="og:type" content="website"></meta>
