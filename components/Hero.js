05257cb007a689175d34608d90ab3506a5aa2e4a
matias <matias.benedetto@gmail.com>
2019-07-13 13:15:58 -0300

    grammar

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ export default () => (
     <div className="hero">
         <div className="container">
             <div className="text">
-                <h1 className="title is-1 is-spaced">Squoosh your images to get the best</h1>
+                <h1 className="title is-1 is-spaced">Squoosh your images to get the best out of it</h1>
                 <p className="subtitle is-5">Multiplatform desktop app to optimize, resize, compress and convert your images.</p>
                 <p className="subtitle is-5">It's completely free and for free!</p>
                 <a href="#download" className="button is-link is-large">Download</a>
