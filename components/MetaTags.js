be25e0ccd594a7dbe7d36d743e556e8db3d42e14
matias <matias.benedetto@gmail.com>
2019-07-12 21:05:28 -0300

    adding favicons

--- components/MetaTags.js
+++ components/MetaTags.js
@@ -22,5 +22,11 @@ export default () => (
         <meta name="og:url" content="https://squoosh-desktop.now.sh/"/>
         <meta name="og:site_name" content="Squoosh Desktop App"/>
         <meta name="og:type" content="website"></meta>
+
+
+        <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png" />
+        <link rel="manifest" href="static/site.webmanifest"></link>
     </Fragment>
 )
