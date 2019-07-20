c0bcd83cbae0adfbb3262fa084bd43cc5ab5d197
matias <matias.benedetto@gmail.com>
2019-07-20 14:43:30 -0300

    fixing image path

--- components/MetaTags.js
+++ components/MetaTags.js
@@ -14,11 +14,11 @@ export default () => (
         <meta name="twitter:card" content="summary"/>
         <meta name="twitter:title" content="Squoosh Desktop App"/>
         <meta name="twitter:description" content="Squoosh your images to get the best out of it | Multiplatform desktop app to optimize, resize, compress and convert your images."/>
-        <meta name="twitter:image:src" content="https://squoosh-desktop.now.sh/gistatic/img/screenshot1.png"/>
+        <meta name="twitter:image:src" content="https://squoosh-desktop.now.sh/static/img/screenshot1.png"/>
 
         <meta name="og:title" content="Squoosh Desktop App"/>
         <meta name="og:description" content="Squoosh your images to get the best out of it | Multiplatform desktop app to optimize, resize, compress and convert your images."/>
-        <meta name="og:image" content="https://squoosh-desktop.now.sh/gistatic/img/screenshot1.png"/>
+        <meta name="og:image" content="https://squoosh-desktop.now.sh/static/img/screenshot1.png"/>
         <meta name="og:url" content="https://squoosh-desktop.now.sh/"/>
         <meta name="og:site_name" content="Squoosh Desktop App"/>
         <meta name="og:type" content="website"></meta>
